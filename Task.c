707e4df2370d90003af358929084fa5e31fc57de
Lychagin228 <anton228lychagin@gmail.com>
2025-12-23 20:30:45 +0300

    Update Task.c

--- Task.c
+++ Task.c
@@ -6,9 +6,11 @@
 // Главная функция
 int main() {
     setlocale(LC_CTYPE, "RUS");
-    int v;
-    double x, y, eps, a, b, step, n;
+    int user_choice; // Переименовали для ясности
+    double x, y, eps, a, b, step;
+    int n;
     double found_x;
+    int search_result;
 
     printf("\n");
     printf("****************************************\n");
@@ -21,11 +23,11 @@ int main() {
     printf("****************************************\n");
 
     do {
-        menu();
-        printf("Выберите действие: ");
-        scanf("%d", &v);
-
-        switch (v) {
+        // Получаем выбор пользователя из меню
+        user_choice = menu();
+        
+        // Обрабатываем выбор
+        switch (user_choice) {
         case 1:
             printf("Введите x: ");
             scanf("%lf", &x);
@@ -47,9 +49,17 @@ int main() {
             scanf("%lf", &a);
             printf("Введите конец интервала: ");
             scanf("%lf", &b);
-            printf("Введите с какой точностью: ");
-            scanf("%lf", &n);
-            printf("Интеграл = %lf\n", integral(a, b, n));
+            if (a > b) {
+                printf("Ошибка: начало интервала должно быть меньше конца!\n");
+                break;
+            }
+            printf("Введите число для нахождения точности (целое число > 0): ");
+            scanf("%d", &n);
+            if (n <= 0) {
+                printf("Ошибка: такое число должно быть положительным!\n");
+                break;
+            }
+            printf("Интеграл = %.6lf\n", integral(a, b, n));
             break;
 
         case 4:
@@ -57,10 +67,18 @@ int main() {
             scanf("%lf", &y);
             printf("Введите точность: ");
             scanf("%lf", &eps);
+            if (eps <= 0) {
+                printf("Ошибка: точность должна быть положительной!\n");
+                break;
+            }
             printf("Поиск x для y = %.3lf\n", y);
-
-            found_x = poisk_x(y, eps);
-            printf("Результат поиска: x = %.4lf, f(x) = %.6lf\n", found_x, f(found_x));
+            
+            search_result = poisk_x(y, eps, &found_x);
+            if (search_result == 1) {
+                printf("Результат поиска: x = %.4lf, f(x) = %.6lf\n", found_x, f(found_x));
+            } else {
+                printf("Решение не найдено на интервале [-10, 10] с заданной точностью.\n");
+            }
             break;
 
         case 5:
@@ -70,20 +88,35 @@ int main() {
             break;
 
         case 6:
-            printf("Выход\n");
+            printf("Выход из программы.\n");
             break;
 
         default:
-            printf("Ошибка выбора\n");
+            printf("Ошибка: неверный выбор операции!\n");
             break;
         }
 
-        if (v != 6) {
-            printf("Продолжить? (1 - да, 0 - нет): ");
-            scanf("%d", &v);
+        // Если не выход, спрашиваем о продолжении
+        if (user_choice != 6) {
+            printf("\nВернуться в меню? (1 - да, 0 - нет): ");
+            int continue_flag;
+            scanf("%d", &continue_flag);
+            if (continue_flag == 0) {
+                user_choice = 6; // Имитируем выбор "Выход"
+                printf("Завершение работы программы.\n");
+            }
         }
 
-    } while (v != 6 && v != 0);
+    } while (user_choice != 6);
 
     return 0;
 }
+case 2:
+            printf("Введите начало интервала: ");
+            scanf("%lf", &a);
+            printf("Введите конец интервала: ");
+            scanf("%lf", &b);
+            printf("Введите шаг: ");
+            scanf("%lf", &step);
+            tablica(a, b, step);
+            break;
